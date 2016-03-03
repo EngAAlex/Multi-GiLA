89daf6abc3082da868fc221a5c79715f5afe0bfe
general <general@generallinux>
2016-03-03 15:19:31 +0100

    Added License information.

--- gila-core/src/unipg/gila/partitioning/PartitionedPositionedVertexValue.java
+++ gila-core/src/unipg/gila/partitioning/PartitionedPositionedVertexValue.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Grafos.ml
+ * Copyright 2014, 2016 Grafos.ml
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
